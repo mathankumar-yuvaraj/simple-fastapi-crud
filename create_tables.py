e20f00925c7e382bfdeb0e595bcee00de13fc7b6
Mathankumar Yuvaraj <mathankumar.yuvaraj@gmail.com>
2026-01-29 23:57:15 +0530

    add create table file

--- create_tables.py
+++ create_tables.py
@@ -1,11 +1,10 @@
-# create_tables.py
 import asyncio
 from database import engine, Base
+
+import models  
 
 async def main():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-
-
 
 asyncio.run(main())
